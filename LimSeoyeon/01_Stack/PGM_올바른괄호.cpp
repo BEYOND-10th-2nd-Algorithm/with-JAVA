69b11d765a8629adc2bcb3a0d2f81569219d79f4
person76 <bhk226@naver.com>
2024-07-31 14:24:47 +0900

    no message

---
+++ LimSeoyeon/01_Stack/PGM_올바른괄호.cpp
@@ -0,0 +1,33 @@
+﻿#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+
+    cin >> s;
+
+    stack <int> st;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            st.push(1);
+        }
+        else {
+            if (st.empty()) {
+                answer = false;
+                break;
+            }
+            else {
+                st.pop();
+            }
+        }
+    }
+
+    if (!st.empty()) {
+        answer = false;
+    }
+    return answer;
+}
