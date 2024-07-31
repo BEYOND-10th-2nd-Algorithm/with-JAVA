8857c5b3e0e3404b84d6123cd43c484933218672
person76 <bhk226@naver.com>
2024-07-31 14:52:36 +0900

    백준 28278 스택2 문제 C++ 코드

---
+++ LimSeoyeon/01_Stack/BOJ_28278_스택2.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	stack<int> st;
+
+	for (int i = 0; i < N; i++) {
+		int a;
+		cin >> a;
+		if (a == 1) {
+			int b; 
+			cin >> b;
+			st.push(b);
+		}
+		else if (a == 2) {
+			if (!st.empty()) {
+				cout << st.top()<<'\n';
+				st.pop();
+			}
+			else {
+				cout << -1<<'\n';
+			}
+		}
+		else if (a == 3) {
+			cout << st.size() << '\n';
+		}
+		else if (a == 4) {
+			cout << st.empty()<< '\n';
+		}
+		else {
+			if (!st.empty()) {
+				cout << st.top() << '\n';
+			}
+			else {
+				cout << -1 << '\n';
+			}
+		}
+	}
+
+}
