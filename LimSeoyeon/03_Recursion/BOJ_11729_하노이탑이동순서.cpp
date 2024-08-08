c9a4d6ea5f4e922bbdd7a9ce989573d4220dc32d
person76 <94957124+person76@users.noreply.github.com>
2024-08-08 17:22:27 +0900

    Create BOJ_11729_하노이탑이동순서.cpp

---
+++ LimSeoyeon/03_Recursion/BOJ_11729_하노이탑이동순서.cpp
@@ -0,0 +1,29 @@
+void recursion(int st, int ext, int dest, int n) {
+
+	if (n == 1) {
+		cout << st << ' ' << dest << '\n';
+		return;
+	}
+	recursion(st, dest, ext, n - 1);
+	cout << st << ' ' << dest << '\n';
+	recursion(ext, st, dest, n - 1);
+}
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	int N;
+	cin >> N;
+	// 1 3 7 15 31 
+	int cnt = 1;
+	for (int i = 1; i < N; i++) {
+		cnt = cnt * 2 + 1;
+	}
+	cout << cnt << '\n';
+	recursion(1, 2, 3, N);
+
+
+}
