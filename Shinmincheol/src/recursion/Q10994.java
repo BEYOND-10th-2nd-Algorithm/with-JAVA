e2f1633dc835c29af69a1a7a8b192e3ea320e151
신민철 <dbeoalscjf98@naver.com>
2024-08-12 16:13:55 +0900

    Q24479 깊이우선탐색1, Q1012 유기농배추

--- Shinmincheol/src/recursion/Q10994.java
+++ Shinmincheol/src/recursion/Q10994.java
@@ -7,7 +7,30 @@ public class Q10994 {
 
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        recursive(n, sb);
+        System.out.println(sb);
+
+
+
+
     }
-    //2n+1
+    //4n+1
     //재귀함수
+    public static void recursive(int n, StringBuilder sb) {
+
+        if(n == 1) {sb.append("*");}
+        else{
+
+            sb.append("****");
+            sb.append("\n");
+            //if()
+            sb.append("*");
+            sb.append("    ");
+            sb.append("*");
+
+
+
+        }
+    }
 }
